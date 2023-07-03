3a8512e91c6a16930f7ac5edf71a675e985f3312
Franz Salisi <fsalisi1@berkeley.edu>
2023-07-03 01:43:08 -0700

    DB initialization and get random word

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
+  const [solution, setSolution] = useState(null)
+  useEffect(() => {
+    fetch('http://localhost:3001/words')
+      .then(response => response.json())
+      .then(jsonFile => {
+        const randomWord = jsonFile[Math.floor(Math.random() * jsonFile.length)]
+        setSolution(randomWord.word)
+      })
+  }, [setSolution]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
