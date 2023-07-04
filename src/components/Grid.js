16436e307ec7d4013c03a4e08e4d49415f51d3f8
Franz Salisi <fsalisi1@berkeley.edu>
2023-07-03 22:30:06 -0700

    Added dynamic for every letter

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,9 @@ export default function Grid({currGuess, guesses, turn}) {
   return (
     <div>{
         guesses.map((guess, i) => { 
+            if (turn === i) {
+                return <Row key={i} currGuess={currGuess}/>
+            }
             return <Row key={i} guess={guess}/>
         })  
     }</div>
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess , currGuess}) {
 
     if (guess) {
         return (
             <div className='row past'>
                 {guess.map((ch, i) => (
                     <div key={i} className= {ch.color}>{ch.key}</div>
+                ))}
+            </div>
+        )
+    }
+
+    if (currGuess) {
+        let letters = currGuess.split('')
+
+        return (
+            <div className='row current'>
+                {letters.map((ch, i) => (
+                    <div key={i} className="filled">{ch}</div>
+                ))}
+                {[...Array(5 - letters.length)].map((ch, i) => (
+                    <div key={i}></div>
                 ))}
             </div>
         )
